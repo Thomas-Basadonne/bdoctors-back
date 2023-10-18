a1a268e4db947c42133a7d4b64574deb24fc2a4f
Thomas-Basadonne <thommybasa@gmail.com>
2023-10-18 16:00:43 +0200

    seeder stars + typologies

---
+++ database/seeders/TypologiesSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypologiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $typologies = [
+            'Cardiologo',
+            'Dermatologo',
+            'Ortopedico',
+            'Pediatria',
+            'Oculista',
+            'Ginecologo',
+            'Neurologo',
+            'Psichiatra',
+            'Chirurgo Generale',
+            'Dentista',
+        ];
+
+        foreach ($typologies as $typology) {
+            DB::table('typologies')->insert([
+                'name' => $typology,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
