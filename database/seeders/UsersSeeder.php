27fbb9fde48840d586388d58c41c8460c0d6db1a
Thomas-Basadonne <thommybasa@gmail.com>
2023-10-18 16:09:37 +0200

    users

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'), // Puoi impostare una password predefinita
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
