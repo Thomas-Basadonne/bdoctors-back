85ffa2405e15ee7ed0bc19fda2e83f37f8b00206
Thomas-Basadonne <thommybasa@gmail.com>
2023-10-18 15:35:21 +0200

    seeder Profiles + Sponsor + Review

--- database/seeders/ReviewsSeeder.php
+++ database/seeders/ReviewsSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class ReviewsSeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class ReviewsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        foreach (range(1, 50) as $index) {
+            DB::table('reviews')->insert([
+                'name' => $faker->firstName,
+                'lastname' => $faker->lastName,
+                'email' => $faker->unique()->safeEmail,
+                'text' => $faker->paragraph,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
--- database/seeders/SponsorshipsSeeder.php
+++ database/seeders/SponsorshipsSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SponsorshipsSeeder extends Seeder
 {
@@ -14,6 +14,32 @@ class SponsorshipsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $sponsorships = [
+            [
+                'name' => '24 ore di sponsorizzazione',
+                'price' => 2.99,
+                'duration' => '24:00:00', // 24 ore
+            ],
+            [
+                'name' => '72 ore di sponsorizzazione',
+                'price' => 5.99,
+                'duration' => '72:00:00', // 72 ore
+            ],
+            [
+                'name' => '144 ore di sponsorizzazione',
+                'price' => 9.99,
+                'duration' => '144:00:00', // 144 ore
+            ],
+        ];
+
+        foreach ($sponsorships as $sponsorship) {
+            DB::table('sponsorships')->insert([
+                'name' => $sponsorship['name'],
+                'price' => $sponsorship['price'],
+                'duration' => $sponsorship['duration'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
