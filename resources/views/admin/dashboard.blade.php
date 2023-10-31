be2b056196956ab002eb7e088ddb564ce2c3915a
Thomas-Basadonne <thommybasa@gmail.com>
2023-10-31 17:18:35 +0100

    add typology url

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,6 +20,8 @@
                         @if (auth()->user()->is_admin)
                             <a href="{{ url('admin/docprofile') }}">{{ __('Vai alla lista dottori') }}</a>
                             <a href="{{ url('admin/docprofile/create') }}">{{ __('crea il tuo profilo da dottore') }}</a>
+                            <a href="{{ url('admin/typology') }}">{{ __('TYPOLOGY') }}</a>
+
                             @else
                             
                             <a href="{{ url('profile/show') }}">{{ __('vai al tuo profilo da dottore') }}</a>
