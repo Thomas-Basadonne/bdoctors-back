a5124baa208546bb643365897f36555953846c0a
AndreaCollura <andrea.collur@gmail.com>
2023-11-03 18:27:43 +0100

    api typology wip

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
--- app/Http/Controllers/Api/TypologyController.php
+++ app/Http/Controllers/Api/TypologyController.php
@@ -6,16 +6,52 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Typology;
 use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
 
 class TypologyController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $query = Typology::with('user');
-        $typologies = $query->get();
-        return response()->json([
-            'success' => true,
-            'results' => $typologies
-        ]);
+        if (!empty($request->all()['id'])) {
+            $array = $request->all()['id'];
+            $userIds = [];
+            foreach ($array as $a) {
+                $userIds[] = DB::table('user_typology')->select('user_id')->where('typology_id', $a)->get();
+            }
+            foreach ($userIds as $type) {
+                foreach ($type as $cc) {
+                    $el[] = $cc->user_id;
+                }
+            }
+            $countEl = array_count_values($el);
+            $userArr = array_keys($countEl, count($array));
+            $data = User::with('typologies')->whereIn('id', $userArr)->get();
+        } else {
+            $data = User::with('typologies')->get();
+        }
+        if (count($data) > 0) {
+            return response()->json([
+                'success' => true,
+                'results' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'il dottore specificato non esiste',
+            ]);
+        }
     }
 }
+
+
+/*  */
+
+
+$query = Typology::with('user');
+$typologies = $query->get();
+return response()->json([
+    'success' => true,
+    'results' => $typologies
+]);
